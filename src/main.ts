27bf8a16df48e1d7aab69e8aa4dd5b7eb41d93e0
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2024-04-28 23:56:03 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { Command, OptionValues } from "commander";
+import {PDFNet} from "@pdftron/pdfnet-node";
+
+const program = new Command();
+
+program
+    .version('0.0.1')
+    .name('Schedule WIEIK - Parser')
+    .usage('yarn start --input [INPUT] --output [INPUT] --config [CONFIG]')
+    .requiredOption('-i, --input [INPUT]', 'path to input file in PDF format')
+    .requiredOption('-o, --output [OUTPUT]', 'path to output file in JSON format')
+    .requiredOption('-c, --config [CONFIG]', 'path to configuration file in YAML format')
+    .requiredOption('-l, --license [LICENSE]', 'license key for Apryse SDK')
+    .parse(process.argv);
+
+const bootstrap = async (options: OptionValues) => {
+    await PDFNet.initialize(options.license)
+
+    const available = await PDFNet.StructuredOutputModule.isModuleAvailable();
+    if (!available) {
+        throw new Error('StructuredOutput module is not available');
+    }
+
+    const excelOutputOptions = new PDFNet.Convert.ExcelOutputOptions();
+    excelOutputOptions.setPages(1, 1);
+
+    await PDFNet.Convert.fileToExcel(options.input, options.output, excelOutputOptions);
+}
+
+bootstrap(program.opts()).catch(console.error)
